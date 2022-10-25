958bfd847b49cd004bf14c3190282f5e8b38d7c3
salimhartono <shalim.hartono@gmail.com>
2022-10-25 14:35:51 +0700

    list doa activty

--- app/src/main/java/com/salim/bitaqwa/menus/doa/ListDoaActivity.kt
+++ app/src/main/java/com/salim/bitaqwa/menus/doa/ListDoaActivity.kt
@@ -7,12 +7,18 @@ import com.salim.bitaqwa.databinding.ActivityListDoaBinding
 class ListDoaActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityListDoaBinding
+    lateinit var title : String
+    var logo : Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityListDoaBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        title = intent.getStringExtra("ext_title").toString()
+        logo = intent.getIntExtra("ext_icon", 0)
+        setSupportActionBar(binding.toolbarListDoa)
 
 
     }
